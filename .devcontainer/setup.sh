d2509da610e4fb0e6b711e6d376288f637f8c141
Bart Janssens <bart@bartjanssens.org>
2026-02-05 21:20:04 +0100

    Stop using Conda

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -10,15 +10,9 @@ openssl genrsa -out config/private.pem 2048
 openssl rsa -in config/private.pem -out config/public.pem -outform PEM -pubout
 yum install -y dos2unix
 dos2unix ./bin/repl
-export PYTHON=""
+
+pip3 install ecmwf-api-client eccodes numpy cdsapi 
+
+export PYTHON="/usr/bin/python3"
 julia +1.10 --project -e 'using Pkg; Pkg.instantiate()'
 julia +1.10 --project /app/backend/setup.jl
-
-/root/.julia/conda/3/x86_64/bin/conda install --no-deps -y libgfortran4
-/root/.julia/conda/3/x86_64/bin/conda install -y genshi
-/root/.julia/conda/3/x86_64/bin/conda install -y eccodes
-/root/.julia/conda/3/x86_64/bin/conda install -y python-eccodes
-/root/.julia/conda/3/x86_64/bin/conda install -y ecmwf-api-client
-/root/.julia/conda/3/x86_64/bin/conda install -y cdsapi
-/root/.julia/conda/3/x86_64/bin/conda install -y numpy
-/root/.julia/conda/3/x86_64/bin/conda install -y fftw
